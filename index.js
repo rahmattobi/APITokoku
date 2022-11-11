c980a3fe403aab9b59d18e680c8cd779b85342cc
rahmattobi <tobytoby298@gmail.com>
2022-11-11 10:25:46 +0700

    setting routes di index

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//panggil router
+var routes = require('./router');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });

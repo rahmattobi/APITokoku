2ede531909523f0edd601a2f6ce1ceb6d98ae9aa
rahmattobi <tobytoby298@gmail.com>
2022-11-11 15:52:35 +0700

    add package jwt

--- router.js
+++ router.js
@@ -3,17 +3,17 @@
 const { json } = require('body-parser');
 
 module.exports = function (app) {
-    var jsonku = require('./controller');
+    var json = require('./controller');
 
-    app.route('/').get(jsonku.index);
+    app.route('/').get(json.index);
 
-    app.route('/get_user').get(jsonku.user);
+    app.route('/get_user').get(json.user);
 
-    app.route('/get_user/:id').get(jsonku.userwithid);
+    app.route('/get_user/:id').get(json.userwithid);
 
-    app.route('/add_user').post(jsonku.adduser);
+    app.route('/add_user').post(json.adduser);
 
-    app.route('/update').put(jsonku.update);
+    app.route('/update').put(json.update);
 
-    app.route('/delete').delete(jsonku.delete);
+    app.route('/delete').delete(json.delete);
 }
